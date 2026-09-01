85903334b69357b697715637aaec5281313811ca
Ayush Mankar <ayush@VICTUS>
2026-09-01 15:28:59 +0530

    Allow SSH access for CI/CD deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ resource "aws_security_group" "app" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-   cidr_blocks = ["1.22.247.40/32"]
+   cidr_blocks  = ["0.0.0.0/0"]
     }
 
   ingress {
